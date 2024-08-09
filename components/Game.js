ff1083adfae9956b3dc27aa6de820deadb8faa0f
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2024-08-08 20:53:04 -0500

    Import p5 at the top of `components/Game.js` and `pages/game.js`
    Remove the `require('p5')` statement inside the `useEffect` hook in `pages/game.js`
    
    **Add WebGL support and shaders**
    * Add `p.WEBGL` to `p.createCanvas` in `components/Game.js` and `pages/game.js`
    * Add `p.preload` method to load shaders in `components/Game.js` and `pages/game.js`
    * Add shader application in `BlackHole` and `Astronaut` classes in `components/Game.js` and `pages/game.js`
    
    **Add error handling**
    * Add try-catch blocks for p5 instance creation and removal in `components/Game.js` and `pages/game.js`
    
    **Update dependencies**
    * Add `webgl` dependency in `package.json`

--- components/Game.js
+++ components/Game.js
@@ -44,8 +44,16 @@ export default function Game() {
         let gold = [];
         let pullStrength = 0.01;
 
+        let blackHoleShader;
+        let astronautShader;
+
+        p.preload = () => {
+          blackHoleShader = p.loadShader('shaders/blackHole.vert', 'shaders/blackHole.frag');
+          astronautShader = p.loadShader('shaders/astronaut.vert', 'shaders/astronaut.frag');
+        };
+
         p.setup = () => {
-          p.createCanvas(p.windowWidth, p.windowHeight);
+          p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
           blackHole = new BlackHole(p.width / 2, p.height / 2);
           astronaut = new Astronaut();
           for (let i = 0; i < 10; i++) {
@@ -77,6 +85,7 @@ export default function Game() {
           }
 
           display() {
+            p.shader(blackHoleShader);
             p.fill(0);
             p.ellipse(this.x, this.y, 100, 100);
           }
@@ -84,13 +93,34 @@ export default function Game() {
 
         class Astronaut {
           constructor() {
-            this.x = p.random(p.width);
-            this.y = p.random(p.height);
+            this.x = 0;
+            this.y = 0;
             this.size = 20;
             this.speed = 2;
+            this.xSpeed = 0;
+            this.ySpeed = 0;
           }
 
           update() {
+            if (p.keyIsDown(p.LEFT_ARROW) || p.keyIsDown(65)) {
+              this.xSpeed = -2;
+            } else if (p.keyIsDown(p.RIGHT_ARROW) || p.keyIsDown(68)) {
+              this.xSpeed = 2;
+            } else {
+              this.xSpeed = 0;
+            }
+
+            if (p.keyIsDown(p.UP_ARROW) || p.keyIsDown(87)) {
+              this.ySpeed = -2;
+            } else if (p.keyIsDown(p.DOWN_ARROW) || p.keyIsDown(83)) {
+              this.ySpeed = 2;
+            } else {
+              this.ySpeed = 0;
+            }
+
+            this.x += this.xSpeed;
+            this.y += this.ySpeed;
+
             let angle = p.atan2(blackHole.y - this.y, blackHole.x - this.x);
             this.x += p.cos(angle) * this.speed;
             this.y += p.sin(angle) * this.speed;
@@ -98,6 +128,7 @@ export default function Game() {
           }
 
           display() {
+            p.shader(astronautShader);
             p.fill(255);
             p.ellipse(this.x, this.y, this.size, this.size);
           }
@@ -142,10 +173,21 @@ export default function Game() {
         }
       };
 
-      const p5Instance = new p5(sketch);
+      let p5Instance;
+      try {
+        p5Instance = new p5(sketch);
+      } catch (error) {
+        console.error('Error creating p5 instance:', error);
+        alert('An error occurred while creating the game. Please try again later.');
+      }
 
       return () => {
-        p5Instance.remove();
+        try {
+          p5Instance.remove();
+        } catch (error) {
+          console.error('Error removing p5 instance:', error);
+          alert('An error occurred while removing the game. Please try again later.');
+        }
       };
     }
   }, []);
--- pages/game.js
+++ pages/game.js
@@ -1,19 +1,26 @@
 import { useEffect } from 'react';
+import p5 from 'p5';
 
 function Game() {
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const p5 = require('p5');
-
       let blackHole;
       let astronaut;
       let trash = [];
       let gold = [];
       let pullStrength = 0.01;
 
+      let blackHoleShader;
+      let astronautShader;
+
       const sketch = (p) => {
+        p.preload = () => {
+          blackHoleShader = p.loadShader('shaders/blackHole.vert', 'shaders/blackHole.frag');
+          astronautShader = p.loadShader('shaders/astronaut.vert', 'shaders/astronaut.frag');
+        };
+
         p.setup = () => {
-          p.createCanvas(p.windowWidth, p.windowHeight);
+          p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
           blackHole = new BlackHole(p.width / 2, p.height / 2);
           astronaut = new Astronaut();
           for (let i = 0; i < 10; i++) {
@@ -45,6 +52,7 @@ function Game() {
           }
 
           display() {
+            p.shader(blackHoleShader);
             p.fill(0);
             p.ellipse(this.x, this.y, 100, 100);
           }
@@ -52,13 +60,34 @@ function Game() {
 
         class Astronaut {
           constructor() {
-            this.x = p.random(p.width);
-            this.y = p.random(p.height);
+            this.x = 0;
+            this.y = 0;
             this.size = 20;
             this.speed = 2;
+            this.xSpeed = 0;
+            this.ySpeed = 0;
           }
 
           update() {
+            if (p.keyIsDown(p.LEFT_ARROW) || p.keyIsDown(65)) {
+              this.xSpeed = -2;
+            } else if (p.keyIsDown(p.RIGHT_ARROW) || p.keyIsDown(68)) {
+              this.xSpeed = 2;
+            } else {
+              this.xSpeed = 0;
+            }
+
+            if (p.keyIsDown(p.UP_ARROW) || p.keyIsDown(87)) {
+              this.ySpeed = -2;
+            } else if (p.keyIsDown(p.DOWN_ARROW) || p.keyIsDown(83)) {
+              this.ySpeed = 2;
+            } else {
+              this.ySpeed = 0;
+            }
+
+            this.x += this.xSpeed;
+            this.y += this.ySpeed;
+
             let angle = p.atan2(blackHole.y - this.y, blackHole.x - this.x);
             this.x += p.cos(angle) * this.speed;
             this.y += p.sin(angle) * this.speed;
@@ -66,6 +95,7 @@ function Game() {
           }
 
           display() {
+            p.shader(astronautShader);
             p.fill(255);
             p.ellipse(this.x, this.y, this.size, this.size);
           }
@@ -110,10 +140,21 @@ function Game() {
         }
       };
 
-      const p5Instance = new p5(sketch);
+      let p5Instance;
+      try {
+        p5Instance = new p5(sketch);
+      } catch (error) {
+        console.error('Error creating p5 instance:', error);
+        alert('An error occurred while creating the game. Please try again later.');
+      }
 
       return () => {
-        p5Instance.remove();
+        try {
+          p5Instance.remove();
+        } catch (error) {
+          console.error('Error removing p5 instance:', error);
+          alert('An error occurred while removing the game. Please try again later.');
+        }
       };
     }
   }, []);
